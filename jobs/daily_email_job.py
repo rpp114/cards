5a56595672c1afa52f94ee5fa6f2a3d6f1cadcd5
Ray Putt <rpputt@hotmail.com>
2018-08-12 20:11:14 -0700

    added daily email job controller.

---
+++ jobs/daily_email_job.py
@@ -0,0 +1,14 @@
+#!/home/ray/project/venv/bin/python3.5
+
+import sys, os, datetime as dt
+
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
+
+from app import db, models, mail, app
+
+from admin_alert import send_admin_email
+
+
+
+
+send_admin_email()
